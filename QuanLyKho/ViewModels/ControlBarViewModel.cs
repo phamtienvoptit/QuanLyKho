5b81fe3730810ee1949ed2a3a72d7a370b033f53
Vo Pham <phamtienvoptit@gmail.com>
2019-07-16 23:00:16 +0700

    Khởi tạo

---
+++ QuanLyKho/ViewModels/ControlBarViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModels
+{
+    public class ControlBarViewModel:BaseViewModel
+    {
+        #region commands
+
+        public ICommand CloseWindowCommand { get; set; }
+
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand=new RelayCommand<UserControl>((p) => { return true;},(p)=>{ });
+        }
+        #endregion
+    }
+}
---
+++ QuanLyKho/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public bool Isloaded = false;
+        public MainViewModel()
+        {
+            if (!Isloaded)
+            {
+                Isloaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+            
+        }
+    }
+}
